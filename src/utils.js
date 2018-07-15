e50750964d67c710d94e53ada36bdd08c1f6e287
izzydoesit <israeldmatos@gmail.com>
2018-07-14 23:22:09 -0700

    Add file handling functions to utils

--- src/utils.js
+++ src/utils.js
@@ -23,16 +23,48 @@ export const delay = time => new Promise((resolve) => {
   setTimeout(() => { resolve(); }, time);
 });
 
+export const fileExists = async (filePath) => {
+  let exists = true;
+  try {
+    fs.accessSync(filePath);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      exists = false;
+    } else {
+      throw err;
+    }
+  }
+  return exists;
+};
+
+export const writeToCsv = ({ headers, records, filePath }) => {
+  const writer = csvWriter({ headers });
+  writer.pipe(fs.createWriteStream(filePath));
+  records.forEach(r => writer.write(r));
+  writer.end();
+};
+
+export const getListFilesDir = () => {
+  let listFilesDir;
+  try {
+    listFilesDir = path.join(__dirname, `../${config.get('listFilesDir')}`);
+    mkdirp.sync(listFilesDir);
+    return listFilesDir;
+  } catch (err) {
+    throw err;
+  }
+};
+
 export const spotExists = async (date) => {
   let exists = false;
   // check pool for spots on day
 
   return exists;
-}
+};
 
 export const bidExists = async (date) => {
   let exists = false;
   // check pool for bids on day
 
   return exists;
-}
+};
