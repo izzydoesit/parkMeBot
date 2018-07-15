3d1516de6d8588c2ead2ea4fd60b3bf8a82f88bc
izzydoesit <israeldmatos@gmail.com>
2018-07-14 21:48:49 -0700

    Improve offer variable names, add parkingSpotList object

--- src/modules/parkingSpots/index.js
+++ src/modules/parkingSpots/index.js
@@ -3,17 +3,24 @@ import config from 'config';
 import { log, delay, spotExists, bidExists } from '../../utils';
 
 // Parking Spots
-import getUserActivity from  './getUserActivity';
+import getParkingBids from  './getParkingBids';
 
 const slackConfig = config.get('slack');
 
 const PARKING_SPOTS_CONFIG = {
   userActivity: {
-    name: 'User Activity',
-    namePrefix: 'userActivity',
+    name: 'Parking Spot Offers',
+    namePrefix: 'parkingSpotOffers',
     type: 'csv',
-    func: getUserActivity
+    func: getParkingOffers
   }
 };
 
 export const parkingSpotList = Object.entries(PARKING_SPOTS_CONFIG)
+  .map(([key, value]) => {
+    const list = {
+      text: value.name,
+      value: key
+    };
+    return list;
+  })
