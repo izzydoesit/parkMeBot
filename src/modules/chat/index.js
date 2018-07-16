38c01c58ef891c431de960904a334eb90e7c38db
izzydoesit <israeldmatos@gmail.com>
2018-07-16 03:14:15 -0700

    Add chat listener on channel join event

--- src/modules/chat/index.js
+++ src/modules/chat/index.js
@@ -12,3 +12,7 @@ controller.hears(
 controller.hears('parking','in_channel', (bot, message) => {
     bot.reply(message,'Did I hear parking? Maybe I can help...');
 });
+
+controller.on('channel_join', (bot, message) => {
+    bot.reply(message,'Welcome to the channel!');
+});
