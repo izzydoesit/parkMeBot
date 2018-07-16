b1b802ded3a73c5ceffa664c1ac6c2ef1149c181
izzydoesit <israeldmatos@gmail.com>
2018-07-15 17:36:24 -0700

    Synthesize separate bid/offer logic into one order func & refs

---
+++ src/modules/parkingOrders/getParkingOrders.js
@@ -0,0 +1,72 @@
+import { log, writeToCsv } from '../../utils';
+
+// GENERATES MOCK RECORDS UP TO AMOUNT OF TOTAL ORDERS
+const generateData = async ({ startDate, endDate, totalOrders, direction }) => {
+  try {
+    const parkingOrders = [];
+    for (let index = 0; index < totalOrders; index += 1) {
+      const rand = Math.floor(Math.random() * 7) + 1;
+      const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+      const randomDay = new Date();
+      randomDay.setDate(randomDay.getDate() + rand);
+
+      parkingOrders.push({
+        id: Math.floor(Math.random() * 25),
+        direction: direction,
+        username: `user_${index + 1}`,
+        day: days[randomDay.getDay()],
+        date: randomDay,
+      });
+    }
+    return parkingOrders;
+  } catch (err) {
+    throw err;
+  }
+};
+
+// GENERATES CSV VILE OF MOCKED ORDERS
+export default async (options) => {
+  console.log('options', options)
+  try {
+    const {
+      startDate = '2018-01-01',
+      endDate = '2018-07-11',
+      totalOrders = 20,
+      orderType,
+      orderReportFilePath,
+    } = options;
+
+    const direction = (orderType == 'offers') ? 'S' : 'B';
+    const parkingOrders = await generateData({
+      startDate,
+      endDate,
+      totalOrders,
+      direction
+    });
+
+    if (parkingOrders.length > 0) {
+      const headers = [
+        'Order Id',
+        'Direction',
+        'Username',
+        'Day',
+        'Date',
+      ];
+
+      const orders = parkingOrders.map(order => [
+        order.id,
+        order.direction,
+        order.username,
+        order.day,
+        order.date,
+      ]);
+
+      const filePath = orderReportFilePath;
+      writeToCsv({ headers, records: orders, filePath });
+      const dir = direction === 'S' ? 'offer' : 'bid';
+      log.info(`${orders.length} parking spot ${dir} orders compiled into ${filePath}`);
+    }
+  } catch (err) {
+    throw err;
+  }
+};
---
+++ src/modules/parkingOrders/index.js
@@ -0,0 +1,171 @@
+import path from 'path';
+import config from 'config';
+
+import {
+  log,
+  delay,
+  offerExists,
+  bidExists,
+  fileExists,
+  getParkingOrderFilesDir,
+} from '../../utils';
+import { postChatMessage, uploadFile } from '../slack';
+// Parking Spots
+import getParkingOrders from './getParkingOrders';
+import getParkingBids from './getParkingBids';
+import getCalendarDays from './getCalendarDays';
+
+const slackConfig = config.get('slack');
+
+const PARKING_ORDERS_CONFIG = {
+  offers: {
+    name: 'Parking Spot Offers',
+    namePrefix: 'parkingOffers',
+    type: 'csv',
+    func: getParkingOrders,
+  },
+  bids: {
+    name: 'Parking Spot Bids',
+    namePrefix: 'parkingBids',
+    type: 'csv',
+    func: getParkingOrders,
+  },
+};
+
+export const orderTypesList = Object.entries(PARKING_ORDERS_CONFIG)
+.map(([key, value]) => {
+  const list = {
+    text: value.name,
+    value: key,
+  };
+  return list;
+});
+
+export const generateOrderReport = async (options) => {
+  try {
+    const { slackReqObj } = options;
+    const orderType = slackReqObj.actions[0].selected_options[0].value;
+    const orderReport = PARKING_ORDERS_CONFIG[orderType];
+
+    if (orderReport === undefined) {
+      const slackReqObjString = JSON.stringify(slackReqObj);
+      log.error(new Error(`orderType: ${orderType} did not match any order types. slackReqObj: ${slackReqObjString}`));
+      const response = {
+        response_type: 'in_channel',
+        text: 'Hmmm :thinking_face: Seems like that order type list is not available. Please try again later as we look into what went wrong.',
+      };
+      return response;
+    }
+
+    const orderReportTmpName = `${orderReport.namePrefix}_${Date.now()}.${orderReport.type}`;
+    const orderReportFilesDir = getParkingOrderFilesDir();
+    const orderReportFilePath = path.join(orderReportFilesDir, orderReportTmpName);
+
+    const orderReportParams = {
+      orderReportName: orderReport.name,
+      orderType,
+      orderReportTmpName,
+      orderReportType: orderReport.type,
+      orderReportFilePath,
+      orderReportFunc() {
+        return orderReport.func({ orderType, orderReportFilePath });
+      }
+    };
+    console.log('params', orderReportParams)
+    // Begin async order report generation
+    generateOrderReportImplAsync(orderReportParams, { slackReqObj });
+
+    const response = {
+      response_type: 'in_channel',
+      text: `Got it :thumbsup: Generating requested order report *${orderReport.name}*\nI'll notify you when I'm done.`,
+      mrkdwn: true,
+      mrkdwn_in: ['text'],
+    };
+    return response;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const generateOrderReportImplAsync = async (options, { slackReqObj }) => {
+  const {
+    orderReportName,
+    orderReportTmpName,
+    orderReportType,
+    orderReportFilePath,
+    orderReportFunc,
+  } = options;
+
+  try {
+    // initiate order report function
+    await orderReportFunc();
+    // delay to ensure previous fs call is done processing file
+    await delay(250);
+    const orderReportExists = await fileExists(orderReportFilePath);
+
+    if (orderReportExists === false) {
+      const message = {
+        responseUrl: slackReqObj.response_url,
+        replaceOriginal: false,
+        text: `There's currently no orders available for *${orderReportName}*`,
+        mrkdwnn: true,
+        mrkdwn_in: ['text'],
+      };
+      return postChatMessage(message)
+        .catch((ex) => {
+          log.error(ex);
+        });
+    }
+    // delay to ensure previous fs call is done processing file
+    await delay(250);
+    const uploadedOrderReport = await uploadFile({
+      filePath: orderReportFilePath,
+      fileTmpName: orderReportTmpName,
+      fileName: orderReportName,
+      fileType: orderReportType,
+      channels: slackConfig.parkMeBot.fileUploadChannel,
+    });
+
+    const message = {
+      responseUrl: slackReqObj.response_url,
+      replaceOriginal: false,
+      text: 'Your order report is ready!',
+      attachmentns: [{
+        text: `<${uploadedOrderReport.file.url_private}|${orderReportName}>`,
+        color: '#2c963f',
+        footer: 'Click order report link to open menu with download option',
+      }],
+    };
+
+    return postChatMessage(message)
+      .catch((err) => {
+        log.error(err);
+      });
+  } catch (err) {
+    log.error(err);
+
+    const message = {
+      responseUrl: slackReqObj.response_url,
+      replaceOriginal: false,
+      text: `Well this is embarassing :sweat: I couldn't successfully get the order report *${orderReportName}*. Please try again later as I look into what went wrong.`,
+      mrkdwn: true,
+      mrkdwn_in: ['text'],
+    };
+    
+    return postChatMessage(message)
+      .catch((ex) => {
+        log.error(ex);
+      });
+  }
+};
+
+// const CALENDAR_DAYS_CONFIG = getCalendarDays({ startDate, endDate, totalDays: 7 })
+
+// export const calendarDaysList = Object.entries(CALENDAR_DAYS_CONFIG)
+// .map(([key, value]) => {
+//   const list = {
+//     text: `${value.day}, ${value.date}`,
+//     value: key,
+//   };
+//   return list;
+// });
