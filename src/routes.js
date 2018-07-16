3fa319f1f3e1b06055d3e2a1f7dcd6272c08fe9d
izzydoesit <israeldmatos@gmail.com>
2018-07-15 17:34:54 -0700

    Add command route to generate parking spot orders csv

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,39 @@
 import express from 'express';
 import { log } from './utils';
 import { 
-  calendarDaysList, generateCalendarDays, 
-  parkingOffersList, parkingBidsList, 
-  generateParkingOffersList, generateParkingBidsList 
-} from './modules/parkingLists';
+  orderTypesList,
+  generateOrderReport,
+  calendarDaysList,
+} from './modules/parkingOrders';
 
 const router = new express.Router();
+
+router.post('/slack/command/parkingspots', async (req, res) => {
+  try {
+    const slackReqObj = req.body;
+    const response = {
+      response_type: 'in_channel',
+      channel: slackReqObj.channel_id,
+      text: 'Hello :slightly_smiling_face:',
+      attachments: [{
+        text: 'What orders would you like to see?',
+        fallback: 'What orders would you like to see?',
+        color: '#2c963f',
+        attachment_type: 'default',
+        callback_id: 'order_type_selection',
+        actions: [{
+          name: 'order_types_select_menu',
+          text: 'choose an order type...',
+          type: 'select',
+          options: orderTypesList,
+        }],
+      }],
+    };
+    return res.json(response);
+  } catch (err) {
+    return res.status(500).send('Something blew up. We\'re looking into it.');
+  }
+});
 
 router.post('/slack/command/parkme', async (req, res) => {
   try {
@@ -20,7 +47,7 @@ router.post('/slack/command/parkme', async (req, res) => {
         fallback: 'What day would you like to park?',
         color: '#2c963f',
         attachment_type: 'default',
-        callback_id: 'offer_selection',
+        callback_id: 'calendar_selection',
         actions: [{
           name: 'date_select_menu',
           text: 'choose a date...',
@@ -35,7 +62,7 @@ router.post('/slack/command/parkme', async (req, res) => {
   }
 });
 
-router.post('/slack/command/sellparking', async (req, res) => {
+router.post('/slack/command/rentspot', async (req, res) => {
   try {
     const slackReqObj = req.body;
     const response = {
@@ -66,8 +93,11 @@ router.post('/slack/actions', async (req, res) => {
   try {
     const slackReqObj = JSON.parse(req.body.payload);
     let response;
+
     if (slackReqObj.callback_id === 'calendar_selection') {
-      response = await generateCalendarDays({ slackReqObj });
+      response = await submitOrder({ slackReqObj });
+    } else if (slackReqObj.callback_id === 'order_type_selection') {
+      response = await generateOrderReport({ slackReqObj });
     }
     return res.json(response);
   } catch (err) {
