f070f09658e4db10b65768e6445595d67a0612d6
izzydoesit <israeldmatos@gmail.com>
2018-07-17 04:31:06 -0700

    Improve syntax

--- src/modules/chat/index.js
+++ src/modules/chat/index.js
@@ -1,18 +1,19 @@
 import Botkit from 'botkit';
 
-const controller = Botkit.anywhere(configuration);
+const controller = Botkit.anywhere(config);
 
 controller.hears(
-    ['hello', 'hi', 'yo', 'what\'s up'],
-    ['direct_message', 'mention', 'direct_mention'], 
-    (bot, message) => {
-    bot.reply(message,'Hello yourself!');
-});
+  ['hello', 'hi', 'yo', 'what\'s up'],
+  ['direct_message', 'mention', 'direct_mention'],
+  (bot, message) => {
+    bot.reply(message, 'Hello yourself!');
+  }
+);
 
-controller.hears('parking','in_channel', (bot, message) => {
-    bot.reply(message,'Did I hear parking? Maybe I can help...');
+controller.hears('parking', 'in_channel', (bot, message) => {
+  bot.reply(message, 'Did I hear parking? Maybe I can help...');
 });
 
 controller.on('channel_join', (bot, message) => {
-    bot.reply(message,'Welcome to the channel!');
+  bot.reply(message, 'Welcome to the channel!');
 });
