9fedd1c02abbea61798847f7ece2cca080eda187
izzydoesit <israeldmatos@gmail.com>
2018-07-17 04:28:25 -0700

    Adjust assigned value, delete unused imported module

--- src/modules/parkingOrders/getCalendarDays.js
+++ src/modules/parkingOrders/getCalendarDays.js
@@ -1,13 +1,12 @@
-import { log } from '../../utils';
 
 export default (totalDays = 7) => {
   const days = [];
   const date = new Date();
   for (let i = 1; i <= totalDays; i += 1) {
     date.setDate(date.getDate() + 1);
-    days.push({ 
+    days.push({
       text: date.toDateString(),
-      value: i.toString(),
+      value: date.toDateString(),
     });
   }
   return days;
